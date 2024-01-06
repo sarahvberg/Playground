c58c6a04035be3b4d06e384138a575de9158fe8e
Sarah Berg <sarah.victoria.berg@entur.org>
2024-01-06 19:08:48 +0100

    Remove 'Todo'-page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import React from 'react';
-import { Routes, Route, Outlet } from 'react-router-dom';
-import { InfoPage } from './InfoPage';
-import { AboutPage } from './pages/AboutPage';
-import { HomePage } from './pages/HomePage';
-import { SideNav } from './components/SideNav';
-import { SmallProjectsPage } from './pages/SmallProjectsPage';
-import { TodoList } from './pages/TodoList';
+import React from "react";
+import { Routes, Route, Outlet } from "react-router-dom";
+import { InfoPage } from "./InfoPage";
+import { AboutPage } from "./pages/AboutPage";
+import { HomePage } from "./pages/HomePage";
+import { SideNav } from "./components/SideNav";
+import { SmallProjectsPage } from "./pages/SmallProjectsPage";
 
 function App() {
   return (
     <div className="fullPage">
       <Routes>
         <Route path="/info" element={<InfoPage />} />
-        <Route path="/" element={<LayoutWithHeader/>}>
-          <Route index element={<HomePage/>} />
+        <Route path="/" element={<LayoutWithHeader />}>
+          <Route index element={<HomePage />} />
           <Route path="about" element={<AboutPage />} />
-          <Route path="todo-list" element={<TodoList />} />
           <Route path="small-projects" element={<SmallProjectsPage />} />
         </Route>
       </Routes>
@@ -28,11 +26,10 @@ function LayoutWithHeader() {
     <>
       <SideNav />
       <main>
-      <Outlet />
+        <Outlet />
       </main>
     </>
   );
 }
-
 
 export default App;
--- src/pages/SmallProjectsPage.tsx
+++ src/pages/SmallProjectsPage.tsx
@@ -2,25 +2,24 @@ import { useState } from "react";
 
 export function SmallProjectsPage() {
   return (
-      <div className="container-fluid">
-          <div className="container">
-              <div className="row">
-                  <div className="col-md-12">
-                      <h1>Small Projects</h1>
-                  </div>
-              </div>
+    <div className="container-fluid">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1>Small Projects</h1>
           </div>
-          <div className="container mt-50">
-            <div className="row">
-              <div className="col-md-4">
-                <Counter/>
-              </div>
-            </div>
-          </div>
+        </div>
       </div>
+      <div className="container mt-50">
+        <div className="row">
+          <div className="col-md-4">
+            <Counter />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
-
 
 function Counter() {
   const [count, setCount] = useState(0); //useState brukes som regel øverst
@@ -34,26 +33,31 @@ function Counter() {
     setCount((prev) => prev + 1);
   }
   function decrease() {
-    if(count !== 0) {
+    if (count !== 0) {
       setCount((prev) => prev - 1);
     }
   }
 
   return (
     <>
-    <div className="counter">
-      <div className="overlay"></div>
-      <h3>Simple Counter</h3>
-      <h2 id="count-el">{count}</h2>
-      {/* <div>{JSON.stringify(zelda)}</div>
+      <div className="counter">
+        <div className="overlay"></div>
+        <h3>Simple Counter</h3>
+        <h2 id="count-el">{count}</h2>
+        {/* <div>{JSON.stringify(zelda)}</div>
       <button id="increment-btn" onClick={incrementAge}>Zelda</button> */}
-      <div className="btns">
-        <button id="increment-btn" onClick={increment}>+</button>
-        <button id="decrease-btn" onClick={decrease}>-</button>
-        <button id="clear-btn" onClick={() => setCount(0)}>CLEAR</button>
+        <div className="btns">
+          <button id="decrease-btn" onClick={decrease}>
+            -
+          </button>
+          <button id="increment-btn" onClick={increment}>
+            +
+          </button>
+          <button id="clear-btn" onClick={() => setCount(0)}>
+            CLEAR
+          </button>
+        </div>
       </div>
-    </div>
     </>
-  )
-  
+  );
 }
